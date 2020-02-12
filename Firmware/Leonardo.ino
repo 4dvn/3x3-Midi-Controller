6f6eb57b9fabc4ae637c321a968c1f8b979b47a3
4dvn <vuha.hau@hyper.vn>
2020-02-12 22:47:08 +0700

    Update Leonardo.ino

--- Firmware/Leonardo.ino
+++ Firmware/Leonardo.ino
@@ -11,15 +11,15 @@ CCPotentiometer potentiometer3(A2, {MIDI_CC::Channel_Volume, CHANNEL_3});
 
 CCPotentiometer potentiometer4(A3, {MIDI_CC::Channel_Volume, CHANNEL_4});
 
-CCPotentiometer potentiometer1(A6, {MIDI_CC::Channel_Volume, CHANNEL_5});
+CCPotentiometer potentiometer5(A6, {MIDI_CC::Channel_Volume, CHANNEL_5});
 
-CCPotentiometer potentiometer2(A7, {MIDI_CC::Channel_Volume, CHANNEL_6});
+CCPotentiometer potentiometer6(A7, {MIDI_CC::Channel_Volume, CHANNEL_6});
 
-CCPotentiometer potentiometer3(A8, {MIDI_CC::Channel_Volume, CHANNEL_7});
+CCPotentiometer potentiometer7(A8, {MIDI_CC::Channel_Volume, CHANNEL_7});
 
-CCPotentiometer potentiometer4(A9, {MIDI_CC::Channel_Volume, CHANNEL_8});
+CCPotentiometer potentiometer8(A9, {MIDI_CC::Channel_Volume, CHANNEL_8});
 
-CCPotentiometer potentiometer4(A10, {MIDI_CC::Channel_Volume, CHANNEL_9});
+CCPotentiometer potentiometer9(A10, {MIDI_CC::Channel_Volume, CHANNEL_9});
 
 void setup() {
   Control_Surface.begin();
